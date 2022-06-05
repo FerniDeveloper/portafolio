16a3eb4066b579030c39fbf9a018e212507e471b
Fernando E. T. Guiarte <fetg.contacto@gmail.com>
2022-06-05 14:42:26 -0500

    Initial Commit

---
+++ assets/js/funciones.js
@@ -0,0 +1,17 @@
+function alerta2(t) {
+  Swal.fire({
+
+    title: '¡Exito!',
+    text: t,
+    icon: 'success',
+    allowOutsideClick: false
+  })
+}
+
+function alertae(t) {
+  Swal.fire({
+    icon: 'error',
+    title: 'Error...',
+    text: t
+  })
+}
